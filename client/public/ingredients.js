89323dfa1df22c16a479a1f59e4e32e564f2e73e
Torie <victoriamthacker@gmail.com>
2023-10-11 13:33:06 -0400

    web scraping func for one uri

--- client/public/ingredients.js
+++ client/public/ingredients.js
@@ -1,48 +1,92 @@
+const host = 'localhost';
+
 var API = (() => {
-    const host = 'localhost';
   
-    var getIngredients = () => {
-        console.log("getIngredients function called");
-      try {
-        fetch("http://" + host + ":8080/api/v1/ingredients", {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          }
-        }).then(resp => resp.json())
-          .then(results => {
-            results.forEach(data => {
-              showRow(data);
-            });
+
+  var getIngredients = () => {
+    console.log("getIngredients function called");
+    try {
+      fetch("http://" + host + ":8080/api/v1/ingredients", {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }).then(resp => resp.json())
+        .then(results => {
+          results.forEach(data => {
+            showRow(data);
           });
-      } catch (e) {
-        console.log(e);
-        console.log('________________________________');
-      }
-  
-      return false;
+        });
+    } catch (e) {
+      console.log(e);
+      console.log('________________________________');
     }
-  
-    function showRow(data) {
-      var ingredientTableEntries = document.getElementById('ingredients-table-entries');
-      var newIngredientRow = document.createElement('tr');
-  
-      // Ingredient name
-      var ingredientCell = document.createElement('td');
-      ingredientCell.textContent = data.name;
-      newIngredientRow.appendChild(ingredientCell);
-  
-      // Tags
-      var tagsCell = document.createElement('td');
-      tagsCell.textContent = data.tags.map(tag => tag.restrictions.join(", ")).join("\n");
-      newIngredientRow.appendChild(tagsCell);
-  
-      ingredientTableEntries.appendChild(newIngredientRow);
+    return false;
+  }
+
+  function showRow(data) {
+    var ingredientTableEntries = document.getElementById('ingredients-table-entries');
+    var newIngredientRow = document.createElement('tr');
+
+    // Ingredient name
+    var ingredientCell = document.createElement('td');
+    ingredientCell.textContent = data.name;
+    newIngredientRow.appendChild(ingredientCell);
+
+    // Tags
+    var tagsCell = document.createElement('td');
+    tagsCell.textContent = data.tags.map(tag => tag.restrictions.join(", ")).join("\n");
+    newIngredientRow.appendChild(tagsCell);
+
+    ingredientTableEntries.appendChild(newIngredientRow);
+  }
+
+  var generateAlternatives = () => {
+    const restriction = document.getElementById("restriction-input").value;
+    const ingredient = document.getElementById("ingredient-input").value;
+    console.log(restriction);
+    console.log(ingredient);
+
+    try {
+      fetch("http://" + host + ":8080/api/v1/ingredients", {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }).then(resp => resp.json())
+        .then(results => {
+          results.forEach(data => {
+            if (data.name == ingredient) {
+              console.log(data.tags[0].alternatives);
+            }
+          });
+        });
+    } catch (e) {
+      console.log(e);
+      console.log('________________________________');
     }
-  
-    return {
-      getIngredients
-    }
-  })();
-  
+
+    return false;
+  }
+
+  return {
+    getIngredients,
+    generateAlternatives
+  }
+})();
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Call the getIngredients function when the page finishes loading
+  API.getIngredients();
+
+  // Add event listener for form submission
+  const form = document.getElementById('restriction-form');
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault(); // Prevent the default form submission
+      API.generateAlternatives();
+    });
+  }
+});
--- server/app.js
+++ server/app.js
@@ -3,27 +3,26 @@ const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const Ingredient = require("./src/models/ingredients_model");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Ingredient = require("./src/models/ingredients_model"); //Need to keep this even though its greyed out
 const Restriction = require("./src/models/dietaryRestrictions_model");
-//const jwt = require('jsonwebtoken');
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// MongoDB connection
+// MongoDB connection (ingredients and restrictions)
 mongoose.connect('mongodb://db:27017/WSIE', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
-
+//test
 app.get("/", (req, res) => {
   res.json({ msg: "data goes here" });
 });
 
-//Ingredients route
+//returns ingredients
 app.get('/api/v1/ingredients', async (req, res) => {
   try {
     const ingredients = await mongoose.model('Ingredient').find();
@@ -35,7 +34,7 @@ app.get('/api/v1/ingredients', async (req, res) => {
   }
 });
 
-//Restrictions route
+//returns restrictions
 app.get('/api/v1/restrictions', async (req, res) => {
   try {
     const restrictions = await mongoose.model('Restriction').find();
@@ -46,5 +45,43 @@ app.get('/api/v1/restrictions', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+// Scraper route - this is for a specific recipe. Need to change so this passes a search
+app.get('/api/v1/scrape-foodnetwork', async (req, res) => {
+  try {
+    //the response variable is what needs to be changed - pass something to this
+    const response = await axios.get('https://www.foodnetwork.com/recipes/giada-de-laurentiis/chicken-florentine-style-recipe-1942850');
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const scrapedData = {};
+
+    //Title of the recipe
+    scrapedData.title = $('.o-AssetTitle__a-Headline').text().replace(/\n/g, '').trim();
+
+    //Recipe ingredients
+    scrapedData.ingredients = [];
+    $('.o-Ingredients__a-Ingredient').each((index, element) => {
+      const ingredientText = $(element).text();
+      const cleanedIngredient = ingredientText.replace(/\n/g, '').trim();
+      if (cleanedIngredient !== 'Deselect All') {
+        scrapedData.ingredients.push(cleanedIngredient);
+      }
+    });
+
+    //Recipe directions
+    scrapedData.directions = [];
+    $('.o-Method__m-Step').each((index, element) => {
+      const directionText = $(element).text().trim();
+      scrapedData.directions.push(directionText);
+    });
+
+    res.json(scrapedData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while scraping data.' });
+  }
+});
+
+
 
 module.exports = app;
