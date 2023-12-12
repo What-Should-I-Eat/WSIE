d02efadcc3ffd66e7117266306b9d807725ce59e
Torie <victoriamthacker@gmail.com>
2023-12-12 11:20:29 -0500

    changed test to search

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  // Render login.pug
+  res.render('login', { title: 'Login' });
+});
+
+// Route to serve login.html from the public directory
+router.get('/login-html', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 module.exports = router;
