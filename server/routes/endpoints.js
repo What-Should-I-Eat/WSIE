d1395165080a61659759e2d829f9c33225612f9e
Torie <victoriamthacker@gmail.com>
2024-01-16 18:00:21 -0500

    not favorites

--- server/routes/endpoints.js
+++ server/routes/endpoints.js
@@ -281,87 +281,87 @@ endpoints.put('/users/:id/favorites', async (req, res) => { //WORKS!
 //---------------------------------------------------------------------------------------------------------------------------------------
 
 //-------------------------------------------------------------Original Endpoints--------------------------------------------------------
-endpoints.get('/ingredients', async (req, res) => {
-    try {
-      const ingredients = await mongoose.model('Ingredient').find();
-      res.json(ingredients);
-    } 
-    catch (error) {
-      console.error('Error fetching ingredients:', error);
-      res.status(500).json({ error: 'ingredients - Internal Server Error' });
-    }
-  });
+// endpoints.get('/ingredients', async (req, res) => {
+//     try {
+//       const ingredients = await mongoose.model('Ingredient').find();
+//       res.json(ingredients);
+//     } 
+//     catch (error) {
+//       console.error('Error fetching ingredients:', error);
+//       res.status(500).json({ error: 'ingredients - Internal Server Error' });
+//     }
+//   });
   
-  //returns restrictions
-  endpoints.get('/restrictions', async (req, res) => {
-    try {
-      const restrictions = await mongoose.model('Restriction').find();
-      res.json(restrictions);
-    } 
-    catch (error) {
-      console.error('Error fetching restrictions:', error);
-      res.status(500).json({ error: 'restrictions - Internal Server Error' });
-    }
-  });
+//   //returns restrictions
+//   endpoints.get('/restrictions', async (req, res) => {
+//     try {
+//       const restrictions = await mongoose.model('Restriction').find();
+//       res.json(restrictions);
+//     } 
+//     catch (error) {
+//       console.error('Error fetching restrictions:', error);
+//       res.status(500).json({ error: 'restrictions - Internal Server Error' });
+//     }
+//   });
   
-  //This is where we want to pass a search so that we can see the options for recipes
-  endpoints.get('/search-simply-recipes/:searchQuery', async (req, res) => {
-    const searchQuery = encodeURIComponent(req.params.searchQuery);
-    console.log("searchQuery = " + searchQuery);
+//   //This is where we want to pass a search so that we can see the options for recipes
+//   endpoints.get('/search-simply-recipes/:searchQuery', async (req, res) => {
+//     const searchQuery = encodeURIComponent(req.params.searchQuery);
+//     console.log("searchQuery = " + searchQuery);
     
-    const url = 'https://www.simplyrecipes.com/search?q=' + searchQuery;
-    console.log("searching url: " + url);
+//     const url = 'https://www.simplyrecipes.com/search?q=' + searchQuery;
+//     console.log("searching url: " + url);
 
-    axios.get(url)
-      .then((response) => {
-        if (response.status === 200) {
-          const html = response.data; //all html from the page
-          const $ = cheerio.load(html);
-          const searchResultsSection = $('.comp.search-results-list-1.card-list'); //list of search results
-          const items = searchResultsSection.find('.comp.card-list__item'); //each item in the list
-          const results = []; //json array to store data
+//     axios.get(url)
+//       .then((response) => {
+//         if (response.status === 200) {
+//           const html = response.data; //all html from the page
+//           const $ = cheerio.load(html);
+//           const searchResultsSection = $('.comp.search-results-list-1.card-list'); //list of search results
+//           const items = searchResultsSection.find('.comp.card-list__item'); //each item in the list
+//           const results = []; //json array to store data
   
-          //Iterating through the list of search results
-          items.each((index, element) => {
-            const title = $(element).find('span.card__title').text().trim(); //Name of the recipe
-            const link = $(element).find('.comp.card').attr('href'); //Link to recipe page
-            results.push({ title, link });
-          });
+//           //Iterating through the list of search results
+//           items.each((index, element) => {
+//             const title = $(element).find('span.card__title').text().trim(); //Name of the recipe
+//             const link = $(element).find('.comp.card').attr('href'); //Link to recipe page
+//             results.push({ title, link });
+//           });
   
-          res.json(results); //json array of recipe names and links 
-        } else {
-          console.error('Request failed with status code', response.status);
-          res.status(500).json({ error: 'Internal Server Error' });
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'search - Internal Server Error' });
-      });
-  });
+//           res.json(results); //json array of recipe names and links 
+//         } else {
+//           console.error('Request failed with status code', response.status);
+//           res.status(500).json({ error: 'Internal Server Error' });
+//         }
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//         res.status(500).json({ error: 'search - Internal Server Error' });
+//       });
+//   });
 
-  endpoints.post("/search-input", async(req, res) => {
-    const recipeInput = new RecipeInput({ input: req.body.input });
-    const savedRecipeInput = await recipeInput.save();
-    res.json(savedRecipeInput);
-  });
+//   endpoints.post("/search-input", async(req, res) => {
+//     const recipeInput = new RecipeInput({ input: req.body.input });
+//     const savedRecipeInput = await recipeInput.save();
+//     res.json(savedRecipeInput);
+//   });
 
-  endpoints.get("/restriction-input", async (req, res) => {
-    try {
-      const restrictionInputs = await mongoose.model('RestrictionInput').find();
-      res.json(restrictionInputs)
-    } 
-    catch (error) {
-      console.error('Error fetching search input:', error);
-      res.status(500).json({ error: 'search input - Internal Server Error' });
-    }
-  });
+//   endpoints.get("/restriction-input", async (req, res) => {
+//     try {
+//       const restrictionInputs = await mongoose.model('RestrictionInput').find();
+//       res.json(restrictionInputs)
+//     } 
+//     catch (error) {
+//       console.error('Error fetching search input:', error);
+//       res.status(500).json({ error: 'search input - Internal Server Error' });
+//     }
+//   });
 
-  endpoints.post("/restriction-input", async(req, res) => {
-    const restrictionInput = new RestrictionInput({ input: req.body.input });
-    const savedRestrictionInput = await restrictionInput.save();
-    res.json(savedRestrictionInput);
-  });
+//   endpoints.post("/restriction-input", async(req, res) => {
+//     const restrictionInput = new RestrictionInput({ input: req.body.input });
+//     const savedRestrictionInput = await restrictionInput.save();
+//     res.json(savedRestrictionInput);
+//   });
 //---------------------------------------------------------------------------------------------------------------------------------------
  
 //-------------------------------------------------------------Edamam Endpoints----------------------------------------------------------
