22b7097421f257c7de81eea4163ef7b8c9f2186a
Torie <victoriamthacker@gmail.com>
2024-02-01 14:13:46 -0500

    Added initial html page and endpoint for forgot password

--- server/src/models/dist/restrictionInput_model.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var mongoose = require('mongoose');
-
-var restrictionInputSchema = new mongoose.Schema({
-  input: String
-});
-var RestrictionInput = mongoose.model('RestrictionInput', restrictionInputSchema);
-module.exports = RestrictionInput;
