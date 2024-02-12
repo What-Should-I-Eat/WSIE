565761bfd65936c79974ab0c91a772feb19878d7
Torie <victoriamthacker@gmail.com>
2024-02-12 10:54:36 -0500

    got rid of duplicated things in forgotPassword

--- server/dist/authentication.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var express = require('express');
-
-var auth = express.Router;
-auth.get('/login', function (req, res, next) {
-  res.render('login');
-});
-module.exports = auth;
--- server/src/models/dietaryRestrictions_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const getModel = require('./generic_model.js');
-
-const RestrictionSchema = new mongoose.Schema({
-    name: String,
-    tags: [String]
-});
-  
-module.exports = getModel('Restriction', RestrictionSchema);
