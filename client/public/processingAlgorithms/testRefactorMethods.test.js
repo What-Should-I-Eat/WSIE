36c219871620ec79aaab0739ec0b8ff58a44d959
Nick Sonsini <nicksonsini97@gmail.com>
2024-02-12 21:05:49 -0700

    add password verification tests

--- client/public/processingAlgorithms/testRefactorMethods.test.js
+++ client/public/processingAlgorithms/testRefactorMethods.test.js
@@ -15,3 +15,27 @@ test('check matching passwords do match', () => {
 test('check different passwords do not match', () => {
     expect(loginHandler2.checkIfPasswordsMatch("not", "matching")).toBe(false);
 });
+
+test('check valid password', () => {
+    expect(loginHandler2.checkIfPasswordIsValid("iAmValid123")).toBe(true);
+});
+
+test('check invalid password - too short', () => {
+    expect(loginHandler2.checkIfPasswordIsValid("123")).toBe(false);
+});
+
+test('check invalid password - too long', () => {
+    expect(loginHandler2.checkIfPasswordIsValid("1234567890123456")).toBe(false);
+});
+
+test('check invalid password - no number', () => {
+    expect(loginHandler2.checkIfPasswordIsValid("noNumberrr")).toBe(false);
+});
+
+test('check invalid password - no lowercase', () => {
+    expect(loginHandler2.checkIfPasswordIsValid("ALLCAPS000")).toBe(false);
+});
+
+test('check invalid password - no capital', () => {
+    expect(loginHandler2.checkIfPasswordIsValid("alllowercase")).toBe(false);
+});
