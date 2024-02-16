ed61e6e96476e9d71beb397c5488d0f0a85521a3
Torie <victoriamthacker@gmail.com>
2024-02-16 14:17:08 -0500

    deleted old things that are not in use

--- server/src/models/restrictionInput_model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const restrictionInputSchema = new mongoose.Schema({
-  input: String,
-});
-
-const RestrictionInput = mongoose.model('RestrictionInput', restrictionInputSchema);
-module.exports = RestrictionInput;
