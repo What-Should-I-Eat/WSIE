4bd1f32bcf750ed662200aeeaf7226046b244fff
Nick Sonsini <nicksonsini97@gmail.com>
2024-05-28 19:01:04 -0600

    added updated colors in commented code for later

--- react-native/my-app/calls/colorConstants.js
+++ react-native/my-app/calls/colorConstants.js
@@ -6,5 +6,19 @@ const mainIndigoButtonBackground = "#002957";
 
 const lightSilverText = "#DFDFDF";
 
+/*
+login
+color: '#155724',
+backgroundColor: '#d4edda',
+borderColor: '#c3e6cb'
+
+grey #4c4b63
+'--primary-color': '#5386e4',
+'--secondary-color': '#4c4b63'
+
+
+
+*/
+
 
 export { appBackgroundColor, mainIndigoButtonBackground, lightSilverText }
