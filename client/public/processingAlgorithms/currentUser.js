7aa1a259fa9b41d5264b103fe44b3772758cb7fe
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-06-04 18:20:18 -0400

    update ip

--- client/public/processingAlgorithms/currentUser.js
+++ client/public/processingAlgorithms/currentUser.js
@@ -1,81 +1,79 @@
 // NOTE: Change the IP here for your localhost, your own network IP, or IP of host VM
-const host = 'http://localhost:8080';
+const host = '';
 
 async function getUserId(username) {
-  try {
-    const response = await fetch(`${host}/api/v1/users/findUserId?username=${username}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.status != 200) {
-      throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(`${host}/api/v1/users/findUserId?username=${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status != 200) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        return "";
     }
-    const data = await response.json();
-    return data;
-  }
-  catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-    return "";
-  }
 }
 
 async function getUserData(username) {
-  try {
-    const response = await fetch(`${host}/api/v1/users/findUserData?username=${username}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.status != 200) {
-      throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(`${host}/api/v1/users/findUserData?username=${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status != 200) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        return "";
     }
-    const data = await response.json();
-    return data;
-  }
-  catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-    return "";
-  }
 }
 
 function getUserNameFromCookie() {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; username=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; username=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 
 function getUserObject(username) {
-  fetch(`${host}/api/v1/users/findUser/${username}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (response.status != 200) {
-        throw new Error('Cannot find user');
-      }
-      return response.json();
+    fetch(`${host}/api/v1/users/findUser/${username}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
     })
-    .then(user => {
-      console.log('User found: ', user);
-      return user;
+        .then(response => {
+            if (response.status != 200) {
+                throw new Error('Cannot find user');
+            }
+            return response.json();
+        })
+        .then(user => {
+            console.log('User found: ', user);
+            return user;
 
-    })
-    .catch(error => {
-      console.error('Fetch error: ', error);
-    });
+        })
+        .catch(error => {
+            console.error('Fetch error: ', error);
+        });
 }
 
 if (typeof module === 'object') {
-  module.exports = {
-    getUserId,
-    getUserData,
-    getUserNameFromCookie,
-    getUserObject
-  }
+    module.exports = {
+        getUserId,
+        getUserData,
+        getUserNameFromCookie,
+        getUserObject
+    }
 }
