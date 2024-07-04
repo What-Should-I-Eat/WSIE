bd387a06963b90a59d3f84346c483375150af645
Robert Thompson <robertdavidthompson632@gmail.com>
2024-07-04 08:50:20 -0400

    Moving legacy client to an archive directory. Also contains the legacy unit tests

--- client/routes/recipe.js
+++ client/routes/recipe.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-const BASE_DIR_PATH = "public/new";
+const BASE_DIR_PATH = "public";
 
 router.get('/', function (_, res) {
   res.sendFile(path.join(__dirname, `../${BASE_DIR_PATH}/recipes/recipes.html`));
