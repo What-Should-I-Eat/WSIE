cecf816f12c3505e1737bbac2378823627fe84c3
CamronOlsen <camronolsen@gmail.com>
2024-10-24 20:12:23 -0500

    Undo unapproved commits
    This reverts commit e14f3bd712f0c25a5ee757723eb3dd7d5a224835, reversing
    changes made to 834619055e140094f2c88f86bb8e49a70e4ebe97.

--- client/routes/recipe.js
+++ client/routes/recipe.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const fetch = require('node-fetch');
 const fileType = require('file-type');
 var path = require('path');
-const Recipe = require('../models/Recipe'); // Assuming you have a Recipe model for MongoDB
 const BASE_DIR_PATH = "public";
 
 router.get('/', function (_, res) {
@@ -14,33 +13,6 @@ router.get('/recipe_details', (_, res) => {
   res.sendFile(path.join(__dirname, `../${BASE_DIR_PATH}/recipes/recipe_details.html`));
 });
 
-// Route to get paginated recipes
-router.get('/api/recipes', async (req, res) => {
-  const { page = 1, limit = 10, search = '' } = req.query;
-  
-  try {
-    const query = {
-      $or: [
-        { name: { $regex: search, $options: 'i' } },
-        { ingredients: { $regex: search, $options: 'i' } }
-      ],
-      published: true // Only fetch published recipes
-    };
-
-    const totalRecipes = await Recipe.countDocuments(query);
-    const recipes = await Recipe.find(query)
-      .skip((page - 1) * limit)
-      .limit(parseInt(limit))
-      .exec();
-
-    res.json({ totalRecipes, recipes });
-  } catch (error) {
-    console.error('Error fetching recipes:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Route to fetch images
 router.get('/get_edamam_image', async (req, res) => {
   const imageUrl = req.query.url;
 
@@ -56,6 +28,5 @@ router.get('/get_edamam_image', async (req, res) => {
     res.status(500).json({ error: "Error occurred fetching image" });
   }
 });
-
 
 module.exports = router;
